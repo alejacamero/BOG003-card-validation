6e2e249412be3acaa6f7e6b94f5da497c0580661
alejacamero <ale.camero777@gmail.com>
2021-06-24 22:38:53 -0500

    Creacion de validator y pruebas con funciones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import validator from './validator.js';
 
 console.log(validator);
+console.log("hola"); 
+/* Insertar en un span un metodo de validador para probar */
+/*function myFunctionUno() {
+    let traerValue = document.getElementById("myText").value;
+    document.getElementById("validador2").innerHTML = validator.maskify(traerValue);
+    document.getElementById("validador3").innerHTML = validator.isValid(traerValue);  
+}; */
+function myFunction() {
+    let traerValue = document.getElementById("myText").value;
+     document.getElementById("mostrar").innerHTML = traerValue;
+   }
+/* 
+Hacer que la funcion isvalid reciba un argumento en string y que lo retorne y que retorne true o false
+luego hacer que la funcion maskify() reciba un argumento en tring y si es isValid es igual a true retorne el string
+Crear en el index.html un imput que reciba un valor que le escribamos el cual va hacer el argumento de isValid
+Hacer un elemento html con un id para mostrar el resultado del maskIfY  
+Segunda versin de lo de arriba despues de haber logrado eso vamos a probar el argoritmo de lumm en isValid
+y con maskifY vamos a crear los pasos para enmascarar ese string que nos llegue como argumento
+*/
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,32 @@
 const validator = {
-  // ...
+  isValid: function(creditCardNumber) {
+    if ( typeof creditCardNumber === "string") {
+      return true
+    } else {
+      return false
+    }
+  }
+  , 
+  maskify: function(creditCardNumber){
+    let str = creditCardNumber;
+    let ultimosDigitos = str.slice(-4);
+    let caracteresRestantes = str.slice(0,-4);
+    let transformacionNumerales = caracteresRestantes.replace(/[1234567890]/gi,'🙀');
+    let stringEnmascarado = transformacionNumerales + ultimosDigitos;
+    console.log(str)
+    console.log(ultimosDigitos);
+    console.log(caracteresRestantes);
+    console.log(transformacionNumerales);
+    console.log(stringEnmascarado);
+    return stringEnmascarado;
+    
+  }
+  ,
+  metodos: function(){
+    return  this.maskify() + " y " + this.isValid();
+  }
+  ,
 };
+
 
 export default validator;
