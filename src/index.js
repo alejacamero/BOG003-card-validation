66db73b3d6176f6059a00d620882c64779be446e
alejacamero <ale.camero777@gmail.com>
2021-07-03 12:20:45 -0500

    Pantalla premiumjs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,48 @@ import validator from './validator.js';
 
 console.log(validator);
 console.log("hola"); 
-document.getElementById("traerText").addEventListener("click",function ejecutarFunciones() {
+
+
+document.getElementById("numberOne").addEventListener("click",function pantallaNumberOne() {
+    document.getElementById("section").innerHTML = `
+        <div class="articulos">
+            <article class="article">
+            <img src="../img/baño-1.png" alt="baño1">
+            </article>
+            <a class= "agendatuCita" href="#">Agenda tu cita</a>
+            <article class="article">
+            <h3>!!Elegiste el baño Number One!!</h3> 
+            <img src="../img/perrito numberone.jpg" alt="perrito pocion uno">
+            <a href="#">Ir a pagar</a>
+            </article>  
+        </div>
+    `;
+
+} );
+
+
+console.log("lineaveintitres");
+
+document.getElementById("premium").addEventListener("click",function pantallaPremium() {
+    document.getElementById("section").innerHTML = `
+        <div class="articulos">
+            <article class="article">
+                <img src="..//img/baño-2.png" alt="baño2">
+            </article>
+            <a class="agendatuCita" href="#">Agenda tu cita</a>
+            <article class="article">
+                <h3>!!Elegiste el baño Premium!!</h3> 
+                <img class="imagenConejito" src="../img/conejitopremium.jpg" alt="Conejito">
+                <a id="irPagar" href="#">Ir a pagar</a>
+            </article>  
+        </div>
+    `;
+
+} );
+
+
+
+/* document.getElementById("traerText").addEventListener("click",function ejecutarFunciones() {
     let traerValue = document.getElementById("myText").value;
     let validarNumero = validator.isValid(traerValue);
     console.log(validarNumero);
@@ -11,7 +52,11 @@ document.getElementById("traerText").addEventListener("click",function ejecutarF
     } else {
         alert("Su tarjeta no es valida, intente nuevamente con un número valido")
     };  
-} );
+} ); */
+
+
+
+   
 
 /* 
 Hacer que la funcion isvalid reciba un argumento en string y que lo retorne y que retorne true o false
